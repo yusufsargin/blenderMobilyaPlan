7832f1df00b322bde53640509ece74b958e68a24
YUSUF <yusufsargin9@gmail.com>
2019-12-07 01:17:34 +0300

    Kutu olusturuldu

--- __init__.py
+++ __init__.py
@@ -1,23 +1,55 @@
 import bpy
 import mathutils
 
-standard_kalınlık = 0.018;
-standard_deinlik = 0.60;
+standard_kalinlik = 0.018;
+standard_derinlik = 0.60;
 standard_yukseklik = 0.75;
 standard_genislik = 0.65;
 obj_sayı = 1;
+sahnedeki_objeler = [];
 
 
-def kasa_Olsutur(kalinlik, derinlik, yukseklik, modul_genislik, isim, collection_adı='Yusuf', yon=1):
+def make_offset(obj, yon=1, ust=0, yan=0):
+    x, y, z = obj.dimensions;
+    if ust is not 0:
+        if yon is 1:
+            obj.dimensions = (x, y, abs(z - (ust * 2)));
+        elif yon is 2:
+            obj.dimensions = (x, abs(y - (ust * 2)), z);
+        elif yon is 3:
+            obj.dimensions = (x, y, abs(z - (ust * 2)));
+    elif yan is not 0:
+        if yon is 1:
+            obj.dimensions = (abs(x - (yan * 2)), y, z);
+        elif yon is 2:
+            obj.dimensions = (abs(x - (yan * 2)), y, z);
+        elif yon is 3:
+            obj.dimensions = (x, abs(y - (yan * 2)), z);
+
+
+def Obje_Olsutur(kalinlik=standard_kalinlik, derinlik=standard_derinlik, yukseklik=standard_yukseklik,
+                 modul_genislik=0, locationX=0, locationY=0, locationZ=0, isim='Sargın', collection_adı='Yusuf',
+                 yon=1):
     bpy.ops.mesh.primitive_cube_add(size=2);
-    sol_yan = bpy.context.active_object;
-    sol_yan.name = str(str(obj_sayı) + '_solyan_' + isim);
+    obj = bpy.context.active_object;
+    obj.name = str(str(len(sahnedeki_objeler)) + '_solyan_' + isim);
+
     if yon is 1:
-        sol_yan.dimensions = (derinlik, kalinlik, yukseklik);
-        sol_yan.location = (-(derinlik / 2), -(kalinlik / 2), (-yukseklik / 2));
+        obj.dimensions = (derinlik, kalinlik, yukseklik);
+        obj.location = (-(derinlik / 2), -(kalinlik / 2), (-yukseklik / 2));
     elif yon is 2:
-        sol_yan.dimensions = (derinlik, modul_genislik, kalinlik);
-        sol_yan.location = (-(derinlik / 2), -(modul_genislik / 2), (-kalinlik / 2));
+        obj.dimensions = (derinlik, modul_genislik, kalinlik);
+        obj.location = (-(derinlik / 2), -(modul_genislik / 2), (-kalinlik / 2));
+    elif yon is 3:
+        obj.dimensions = (kalinlik, modul_genislik, yukseklik);
+        obj.location = (-(kalinlik / 2), -(modul_genislik / 2), -(yukseklik / 2));
+
+    if (locationX is not 0) or (locationY is not 0) or (locationZ is not 0):
+        x, y, z = obj.location;
+        obj.location = ((x + locationX), (y + locationY), (z + locationZ));
+
+    sahnedeki_objeler.append(obj);
+    return obj
 
 
 def collection_move(x, y, z, collection_adı):
@@ -29,7 +61,14 @@ def collection_move(x, y, z, collection_adı):
         obj.location = (int(x + old_locationX), int(y + old_locationY), int(z + old_locationZ));
 
 
+def kutu_Olustur():
+    sol_yan = Obje_Olsutur(kalinlik=1.8, derinlik=45, yukseklik=75, yon=1);
+    sag_yan = Obje_Olsutur(kalinlik=1.8, derinlik=45, yukseklik=75, locationY=-60, yon=1);
+    ust_tabla = Obje_Olsutur(kalinlik=1.8, derinlik=45, modul_genislik=(60 - 1.8), locationY=-1.8, yon=2);
+    alt_tabla = Obje_Olsutur(kalinlik=1.8, derinlik=45, modul_genislik=60, locationZ=-65, locationY=-(0.9), yon=2);
+    arkalik = Obje_Olsutur(kalinlik=0.8, yukseklik=75, modul_genislik=60, yon=3);
+    on_Kapak = Obje_Olsutur(kalinlik=1.8, yukseklik=65, modul_genislik=60, yon=3, locationX=-45, locationY=-0.9);
+
+
 if __name__ == '__main__':
-    kasa_Olsutur(1.8, 65, 75, 70, 'Deneme1', yon=2);
-    kasa_Olsutur(1.8, 65, 75, 70, 'Deneme1', yon=1);
-    # collection_move(-9, -9, 65, 'Yusuf');
+    kutu_Olustur();
