f51cc1e4a4075af2da1600b2bfebad16fe9d59a7
YUSUF <yusufsargin9@gmail.com>
2019-12-06 14:21:55 +0300

    sıfırlama yapıldı

--- __init__.py
+++ __init__.py
@@ -8,33 +8,28 @@ standard_genislik = 0.65;
 obj_sayı = 1;
 
 
-def kasaOlsutur(kalinlik, derinlik, yukseklik, modul_genislik, isim, collection_adı='Yusuf', yon=1):
+def kasa_Olsutur(kalinlik, derinlik, yukseklik, modul_genislik, isim, collection_adı='Yusuf', yon=1):
     bpy.ops.mesh.primitive_cube_add(size=2);
     sol_yan = bpy.context.active_object;
     sol_yan.name = str(str(obj_sayı) + '_solyan_' + isim);
     if yon is 1:
         sol_yan.dimensions = (derinlik, kalinlik, yukseklik);
+        sol_yan.location = (-(derinlik / 2), -(kalinlik / 2), (-yukseklik / 2));
     elif yon is 2:
         sol_yan.dimensions = (derinlik, modul_genislik, kalinlik);
+        sol_yan.location = (-(derinlik / 2), -(modul_genislik / 2), (-kalinlik / 2));
 
 
 def collection_move(x, y, z, collection_adı):
     objects_sahne = bpy.data.collections[collection_adı].objects;
 
     for obj in objects_sahne:
-        obj.location = (x, y, z);
-
-
-def cursor_koseye_sıfırlama():
-    bpy.ops.object.mode_set(mode='OBJECT');
-    obj = bpy.context.active_object;
-    bpy.ops.object.mode_set(mode='EDIT');
-    bpy.ops.mesh.select_mode(type='VERT');
-    bpy.ops.mesh.select_all(action='DESELECT');
-
+        old_locationX, old_locationY, old_locationZ = obj.location;
+        print(old_locationX)
+        obj.location = (int(x + old_locationX), int(y + old_locationY), int(z + old_locationZ));
 
 
 if __name__ == '__main__':
-    kasaOlsutur(1.8, 65, 75, 70, 'Deneme1', yon=1);
-    kasaOlsutur(1.8, 65, 75, 70, 'Deneme1', yon=2);
-    collection_move(1, 5, 2, 'Yusuf')
+    kasa_Olsutur(1.8, 65, 75, 70, 'Deneme1', yon=2);
+    kasa_Olsutur(1.8, 65, 75, 70, 'Deneme1', yon=1);
+    # collection_move(-9, -9, 65, 'Yusuf');
