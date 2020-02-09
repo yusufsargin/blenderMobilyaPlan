e8c62c992c99815b3c514969fc8d8907510766d8
YUSUF <yusufsargin9@gmail.com>
2020-02-09 18:52:51 +0300

    firebase Connection

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import pyrebase
 
 
 class Collection:
@@ -10,23 +11,67 @@ class Collection:
         self.renderedData = {}
         self.shortedData = []
 
+        self.config = {
+            "apiKey": "AIzaSyDN2GcZbIwk5wpW-7KkWYPRN_Yf1KBJnQk",
+            "authDomain": "blender-44440.firebaseapp.com",
+            "databaseURL": "https://blender-44440.firebaseio.com",
+            "projectId": "blender-44440",
+            "storageBucket": "",
+            "messagingSenderId": "625093549043",
+            "appId": "1:625093549043:web:fb06aaeae958bd44a15086"
+        }
+
+        self.firebase = pyrebase.initialize_app(self.config)
+
     def getDataFromDatabase(self):
-        data = json.loads(requests.get('https://sarginapi.herokuapp.com/renders').text);
+        #data = json.loads(requests.get('https://sarginapi.herokuapp.com/renders').text);
+        db = self.firebase.database()
+
+        blenderModel = db.child("wixData").get().val()
 
         if self.filter == 'renderItem':
+            for data in blenderModel:
+                item = (dict(db.child('wixData').child(data).get().val()))
+                if item.get('rendered'):
+                    for elementItem in item.get('cizim'):
+                        self.databaseItems.append(elementItem);
+
+                    self.customerProperty.append({
+                        'databaseId': item.get('wixId'),
+                        'musteriAdi': item.get('musteriAdi'),
+                        'musteriEmail': item.get('musteriEmail'),
+                        'time': int(item.get('time')),
+                        'databaseItems': self.databaseItems
+                    })
+        else:
+            for data in blenderModel:
+                item = dict(db.child('wixData').child(data).get().val())
+                if not item.get('rendered'):
+                    for elementItem in item.get('cizim'):
+                        self.databaseItems.append(elementItem);
+
+                    self.customerProperty.append({
+                        'databaseId': item.get('wixId'),
+                        'musteriAdi': item.get('musteriAdi'),
+                        'musteriEmail': item.get('musteriEmail'),
+                        'time': int(item.get('time')),
+                        'databaseItems': self.databaseItems
+                    })
+
+        """if self.filter == 'renderItem':
             for item in data:
-                if item['data'].get('rendered'):
+                if item.get('rendered'):
                     self.databaseItems = []
-                    for element in item['data']['cizim']:
+                    for element in item['cizim']:
                         self.databaseItems.append(element);
 
                     self.customerProperty.append({
-                        'databaseId': item['data'].get('wixId'),
-                        'musteriAdi': item['data'].get('musteriAdi'),
-                        'musteriEmail': item['data'].get('musteriEmail'),
-                        'time': int(item.get('data').get('time')),
+                        'databaseId': item.get('wixId'),
+                        'musteriAdi': item.get('musteriAdi'),
+                        'musteriEmail': item.get('musteriEmail'),
+                        'time': int(item.get('time')),
                         'databaseItems': self.databaseItems
-                    });
+                    });"""
 
         if len(self.customerProperty) > 1:
             for i in range(1, len(self.customerProperty)):
@@ -44,7 +89,10 @@ class Collection:
         print('\n');
 
     def changeRenderStatus(self, id):
-        requests.post('https://sarginapi.herokuapp.com/renders/update/' + id, {'renderedStatus': False})
+        db = self.firebase.database()
+
+        db.child('wixData').child(id).update({"rendered": "false"})
+        # requests.post('https://sarginapi.herokuapp.com/renders/update/' + id, {'renderedStatus': False})
 
 
 if __name__ == '__main__':
